2015804a2a6bd85722116a3b9a694112a7b6c66e
BuGeeSUN-Venus <41279781+BuGeeSUN-Venus@users.noreply.github.com>
2020-09-08 17:56:21 +0800

    Revert "Revert "SunDC""
    This reverts commit fa27c5e54b40941d695feafd73b68c2473e009e3.

---
+++ src/main/java/com/example/sdc_jedis_pool/SdcJedisPoolApplication.java
@@ -0,0 +1,13 @@
+package com.example.sdc_jedis_pool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdcJedisPoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdcJedisPoolApplication.class, args);
+    }
+
+}
